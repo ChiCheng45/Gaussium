4160ab1e87f14a8aaf753bf9c464dc3d93ac369b
Chi Cheng <chicheng85@gmail.com>
2015-09-14 22:35:44 +0800

    First commit

--- QChem/src/__init__.py
+++
@@ -1,5 +0,0 @@
-from QChem.src.Nuclei import Nuclei
-from QChem.src.CoulombsLaw import Coulomb
-from QChem.src.FileInputNuclei import FileInputNuclei
-from QChem.src.FileInputBasis import FileInputBasis
-from QChem.src.CreateMatrix import Matrix
