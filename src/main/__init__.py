68bef7385aa6e07033ae499320131403d8ab78bb
Chi Cheng <chicheng85@gmail.com>
2015-09-24 09:19:40 +0800

    Finish some tests and made more changes.

--- src/main/__init__.py
+++ src/main/__init__.py
@@ -1,10 +1,11 @@
-__all__ = ['Coulomb', 'Matrix', 'FileInputNuclei', 'FileInputBasis', 'KineticEnergyIntegral',
+__all__ = ['CoulombTotal', 'Coulomb',  'Matrix', 'FileInputNuclei', 'FileInputBasis', 'KineticEnergyIntegral',
            'Nuclei', 'OverlapIntegral']
 
-from src.main.CoulombsLaw import Coulomb
-from src.main.CreateMatrix import Matrix
-from src.main.FileInputNuclei import FileInputNuclei
-from src.main.FileInputBasis import FileInputBasis
-from src.main.KineticEnergyIntegral import KineticEnergyIntegral
-from src.main.Nuclei import Nuclei
-from src.main.OverlapIntegral import OverlapIntegral
+from src.main.coulombslaw import CoulombTotal
+from src.main.coulombslaw import Coulomb
+from src.main.creatematrix import Matrix
+from src.main.fileinputnuclei import FileInputNuclei
+from src.main.fileinputbasis import FileInputBasis
+from src.main.kineticenergyintegral import KineticEnergyIntegral
+from src.main.nuclei import Nuclei
+from src.main.overlapintegral import OverlapIntegral
