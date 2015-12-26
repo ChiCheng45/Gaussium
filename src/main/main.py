8ea90c2d5711c0f280cf41a126dc0b13e530afb2
Chi Cheng <chicheng85@gmail.com>
2015-12-26 22:37:46 +0800

    .

--- src/main/main.py
+++ src/main/main.py
@@ -2,4 +2,4 @@ from src.main.hartreefock import HartreeFock
 
 if __name__ == "__main__":
     HartreeFock.begin('O2.mol', 'STO-3G.gbs', 'UHF')
-    # HartreeFock.begin('O2.mol', 'STO-3G.gbs', 'RHF')
+    HartreeFock.begin('O2.mol', 'STO-3G.gbs', 'RHF')
