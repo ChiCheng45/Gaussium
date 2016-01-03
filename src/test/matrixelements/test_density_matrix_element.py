3f072a39d79db7f7bb57c9e6183659fefaeef369
Chi Cheng <chicheng85@gmail.com>
2016-01-03 12:54:30 +0800

    fixed tests

--- src/test/matrixelements/test_density_matrix_element.py
+++ src/test/matrixelements/test_density_matrix_element.py
@@ -7,20 +7,19 @@ import numpy as np
 class TestDensityElementRestricted(TestCase):
 
     def setUp(self):
-        self.electrons = 2
-        self.orbital_coefficient_matrix = np.matrix([[0.9291, -0.6259], [0.1398, 1.1115]])
+        self.density_matrix_element = DensityMatrixRestricted()
+        self.density_matrix_element.electrons = 2
+        self.density_matrix_element.matrix_size = 2
+        self.density_matrix_element.orbital_coefficient = np.matrix([[0.9291, -0.6259], [0.1398, 1.1115]])
 
     def test_method_calculate_returns_density_matrix_element_for_0_0(self):
-        density_matrix_element = DensityMatrixRestricted(self.orbital_coefficient_matrix, self.electrons)
-        element = density_matrix_element.calculate(0, 0)
+        element = self.density_matrix_element.calculate_restricted(0, 0)
         testing.assert_approx_equal(element, 1.7266, 4)
 
     def test_method_calculate_returns_density_matrix_element_for_1_0(self):
-        density_matrix_element = DensityMatrixRestricted(self.orbital_coefficient_matrix, self.electrons)
-        element = density_matrix_element.calculate(1, 0)
+        element = self.density_matrix_element.calculate_restricted(1, 0)
         testing.assert_approx_equal(element, 0.2599, 3)
 
     def test_method_calculate_returns_density_matrix_element_for_1_1(self):
-        density_matrix_element = DensityMatrixRestricted(self.orbital_coefficient_matrix, self.electrons)
-        element = density_matrix_element.calculate(1, 1)
+        element = self.density_matrix_element.calculate_restricted(1, 1)
         testing.assert_approx_equal(element, 0.0391, 3)
