863396043b69b046e6e5c65df66e7822d1832690
Chi Cheng <chicheng85@gmail.com>
2016-04-09 14:04:22 +0800

    cleaning

--- src/main/matrixelements/g_matrix.py
+++ src/main/matrixelements/g_matrix.py
@@ -11,8 +11,6 @@ class GMatrix(Matrix):
         self.density_matrix_alph = np.matrix([])
         self.density_matrix_beta = np.matrix([])
         self.density_matrix_total = np.matrix([])
-        self.electrons_alph = 0
-        self.electrons_beta = 0
 
     def calculate_restricted(self, i, j):
         g_ij = 0
@@ -40,28 +38,6 @@ class GMatrix(Matrix):
                 exchange = self.density_matrix_beta.item(a, b) * self.repulsion_matrix.item(i, b, a, j)
                 g_ij += coulomb - exchange
         return g_ij
-
-    def calc_constrained_alph(self, i, j):
-        # (i is virtual and j is open or closed) or (i is virtual and j is open or closed)
-        if i > self.electrons_alph and j < (self.electrons_alph or self.electrons_beta) \
-        or j > self.electrons_alph and i < (self.electrons_alph or self.electrons_beta):
-
-            return self.calculate_restricted(i, j)
-
-        else:
-
-            return self.calc_unrestricted_alph(i, j)
-
-    def calc_constrained_beta(self, i, j):
-        # (i is virtual and j is open or closed) or (i is virtual and j is open or closed)
-        if i > self.electrons_beta and j < (self.electrons_alph or self.electrons_beta) \
-        or j > self.electrons_beta and i < (self.electrons_alph or self.electrons_beta):
-
-            return self.calculate_restricted(i, j)
-
-        else:
-
-            return self.calc_unrestricted_beta(i, j)
 
 
 class GMatrixRestricted(GMatrix):
@@ -93,3 +69,25 @@ class GMatrixConstrainedUnrestricted(GMatrix):
         self.density_matrix_beta = density_matrix_beta
         self.density_matrix_total = density_matrix_alph + density_matrix_beta
         return self.create_matrix(self.calc_constrained_alph), self.create_matrix(self.calc_constrained_beta)
+
+    def calc_constrained_alph(self, i, j):
+        # (i is virtual and j is open or closed) or (i is virtual and j is open or closed)
+        if i > self.electrons_alph and j < (self.electrons_alph or self.electrons_beta) \
+        or j > self.electrons_alph and i < (self.electrons_alph or self.electrons_beta):
+
+            return self.calculate_restricted(i, j)
+
+        else:
+
+            return self.calc_unrestricted_alph(i, j)
+
+    def calc_constrained_beta(self, i, j):
+        # (i is virtual and j is open or closed) or (i is virtual and j is open or closed)
+        if i > self.electrons_beta and j < (self.electrons_alph or self.electrons_beta) \
+        or j > self.electrons_beta and i < (self.electrons_alph or self.electrons_beta):
+
+            return self.calculate_restricted(i, j)
+
+        else:
+
+            return self.calc_unrestricted_beta(i, j)
