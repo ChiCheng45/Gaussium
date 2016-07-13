2cec5add08069c5adb79a48ac4c9807028374709
Chi Cheng <chicheng85@gmail.com>
2016-07-14 01:28:28 +0800

    cleanup

--- src/main/integrals/boys_function.py
+++ src/main/integrals/boys_function.py
@@ -4,7 +4,27 @@ from numba import jit
 
 @jit
 def boys_function(v, x):
+    """Computes the boys function used for calculating the two electron and nuclear attraction integrals.
 
+    Parameters
+    ----------
+    v : {int, float}
+    x : {int, float}
+
+    Returns
+    -------
+    ans : float
+
+    Notes
+    -----
+    Added @jit decorator to give a small speed up. There are also no checks for if the series diverges and causes an
+    infinite loop.
+
+    References
+    ----------
+    [1] Handbook of Computational Chemistry pg. 280
+
+    """
     # Approximation of the boys function for small x
     if x <= 25:
         i = 0
@@ -34,4 +54,21 @@ def boys_function(v, x):
 
 
 def boys_function_recursion(v, x, f_v):
+    """Returns the answer to the boys function f_{v - 1}(x) using the answer for the boys function f_{v}(x).
+
+    Parameters
+    ----------
+    v : {int, float}
+    x : {int, float}
+    f_v : float
+
+    Returns
+    -------
+    : float
+
+    References
+    ----------
+    [1] Handbook of Computational Chemistry pg. 280
+
+    """
     return (exp(-x) + 2 * x * f_v) / (2 * v - 1)
