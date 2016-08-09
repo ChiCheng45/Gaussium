957d9fb3e4abc0728777fe663f81f46c49100141
Chi Cheng <chicheng85@gmail.com>
2016-08-09 12:29:56 +0800

    small change

--- src/main/kohnsham/integration.py
+++ src/main/kohnsham/integration.py
@@ -10,19 +10,21 @@ class ExchangeCorrelation:
         self.correlation_potential = correlation_potential
         self.int_space = int_space
         self.density_matrix = np.matrix([])
-        self.electron_density_memo = {}
+        self.electron_density = {}
+        self.integrand = {}
 
     def integrate(self, density_matrix, i, j):
 
         if density_matrix is not self.density_matrix:
             self.density_matrix = density_matrix
-            self.electron_density_memo = {}
+            self.electron_density = {}
+            self.integrand = {}
 
         g_i = self.basis_set[i]
         g_j = self.basis_set[j]
 
         def electron_density(x, y, z):
-            if (x, y, z) not in self.electron_density_memo:
+            if (x, y, z) not in self.electron_density:
                 density = 0
                 for a, basis_a in enumerate(self.basis_set):
                     for b, basis_b in enumerate(self.basis_set):
@@ -30,21 +32,23 @@ class ExchangeCorrelation:
                             density += density_matrix.item(a, b) * basis_a.value(x, y, z)**2
                         elif a <= b:
                             density += 2 * basis_a.value(x, y, z) * density_matrix.item(a, b) * basis_b.value(x, y, z)
-                self.electron_density_memo[(x, y, z)] = density
-            return self.electron_density_memo[(x, y, z)]
+                self.electron_density[(x, y, z)] = density
+            return self.electron_density[(x, y, z)]
 
         def integrand(x, y, z):
-            if i == j:
-                return g_i.value(x, y, z)**2 * (self.exchange_potential.calculate(electron_density(x, y, z))
-                + self.correlation_potential.calculate(electron_density(x, y, z)))
-            else:
-                return g_i.value(x, y, z) * (self.exchange_potential.calculate(electron_density(x, y, z))
-                + self.correlation_potential.calculate(electron_density(x, y, z))) * g_j.value(x, y, z)
+            if (x, y, z) not in self.integrand:
+                if i == j:
+                    self.integrand[(x, y, z)] = g_i.value(x, y, z)**2 \
+                    * (self.exchange_potential.calculate(electron_density(x, y, z))
+                    + self.correlation_potential.calculate(electron_density(x, y, z)))
+                else:
+                    self.integrand[(x, y, z)] = g_i.value(x, y, z) * g_j.value(x, y, z) \
+                    * (self.exchange_potential.calculate(electron_density(x, y, z))
+                    + self.correlation_potential.calculate(electron_density(x, y, z)))
+            return self.integrand[(x, y, z)]
 
         integral, error = integrate.nquad(integrand, [[-self.int_space, self.int_space],
         [-self.int_space, self.int_space], [-self.int_space, self.int_space]],
-        opts=[{'epsabs': 1e-3, 'epsrel': 0},
-              {'epsabs': 1e-3, 'epsrel': 0},
-              {'epsabs': 1e-3, 'epsrel': 0}])
+        opts=[{'epsabs': 1e-3, 'epsrel': 0}, {'epsabs': 1e-3, 'epsrel': 0}, {'epsabs': 1e-3, 'epsrel': 0}])
 
         return integral
