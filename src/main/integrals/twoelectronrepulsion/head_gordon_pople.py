85986d033cc11ac627e1f7bfa09bc723d3d4161a
Chi Cheng <chicheng85@gmail.com>
2016-08-14 12:58:47 +0800

    small change

--- src/main/integrals/twoelectronrepulsion/head_gordon_pople.py
+++ src/main/integrals/twoelectronrepulsion/head_gordon_pople.py
@@ -1,6 +1,7 @@
 from src.main.common import coordinate_distance
 from src.main.common import gaussian_product_coordinate
 from src.main.integrals import boys_function
+from src.main.integrals import boys_function_recursion
 from src.main.objects import PrimitiveBasis
 from math import sqrt, pi, exp
 
@@ -8,17 +9,49 @@ from math import sqrt, pi, exp
 class HeadGordonPople:
 
     def __init__(self):
+        self.a_7 = 0
+        self.r_7 = ()
         self.end_dict = {}
 
     def integrate(self, g1, g2, g3, g4):
-        self.end_dict = {}
-        return self.hgp_sort(g1, g2, g3, g4)
-
-    def hgp_sort(self, g1, g2, g3, g4):
         l_1 = g1.integral_exponents
         l_2 = g2.integral_exponents
         l_3 = g3.integral_exponents
         l_4 = g4.integral_exponents
+        l_total = sum(l_1) + sum(l_2) + sum(l_3) + sum(l_4)
+
+        a_1 = g1.exponent
+        a_2 = g2.exponent
+        a_3 = g3.exponent
+        a_4 = g4.exponent
+        a_5 = a_1 + a_2
+        a_6 = a_3 + a_4
+        self.a_7 = (a_5 * a_6) / (a_5 + a_6)
+
+        r_1 = g1.coordinates
+        r_2 = g2.coordinates
+        r_3 = g3.coordinates
+        r_4 = g4.coordinates
+        r_5 = gaussian_product_coordinate(a_1, r_1, a_2, r_2)
+        r_6 = gaussian_product_coordinate(a_3, r_3, a_4, r_4)
+        self.r_7 = gaussian_product_coordinate(a_5, r_5, a_6, r_6)
+
+        r_12 = coordinate_distance(r_1, r_2)
+        r_34 = coordinate_distance(r_3, r_4)
+        r_56 = coordinate_distance(r_5, r_6)
+
+        boys_x = (a_5 * a_6 * r_56**2) / (a_5 + a_6)
+        boys_out1 = (2 * pi**(5/2)) / (a_5 * a_6 * sqrt(a_5 + a_6))
+        boys_out2 = exp(((- a_1 * a_2 * r_12**2) / a_5) - ((a_3 * a_4 * r_34**2) / a_6))
+        boys_out3 = boys_function(l_total, boys_x)
+
+        self.end_dict = {l_total: boys_out1 * boys_out2 * boys_out3}
+
+        while l_total >= 1:
+            boys_out3 = boys_function_recursion(l_total, boys_x, boys_out3)
+            l_total -= 1
+            self.end_dict[l_total] = boys_out1 * boys_out2 * boys_out3
+
         if sum(l_1) >= sum(l_2) and sum(l_3) >= sum(l_4):
             return self.hgp_begin_horizontal(g1, g2, g3, g4)
         elif sum(l_1) >= sum(l_2):
@@ -32,25 +65,45 @@ class HeadGordonPople:
         l_2 = g2.integral_exponents
         l_4 = g4.integral_exponents
         if l_2[0] > 0:
-            return self.horizontal_recursion(0, *self.hgp_horizontal_factory(0, g1, g2, g3, g4))
+            return self.horizontal_recursion(0, g1, g2, g3, g4)
         elif l_2[1] > 0:
-            return self.horizontal_recursion(1, *self.hgp_horizontal_factory(1, g1, g2, g3, g4))
+            return self.horizontal_recursion(1, g1, g2, g3, g4)
         elif l_2[2] > 0:
-            return self.horizontal_recursion(2, *self.hgp_horizontal_factory(2, g1, g2, g3, g4))
+            return self.horizontal_recursion(2, g1, g2, g3, g4)
         elif l_4[0] > 0:
-            return self.horizontal_recursion(0, *self.hgp_horizontal_factory(0, g3, g4, g1, g2))
+            return self.horizontal_recursion(0, g3, g4, g1, g2)
         elif l_4[1] > 0:
-            return self.horizontal_recursion(1, *self.hgp_horizontal_factory(1, g3, g4, g1, g2))
+            return self.horizontal_recursion(1, g3, g4, g1, g2)
         elif l_4[2] > 0:
-            return self.horizontal_recursion(2, *self.hgp_horizontal_factory(2, g3, g4, g1, g2))
+            return self.horizontal_recursion(2, g3, g4, g1, g2)
         else:
             return self.hgp_begin_vertical(0, g1, g2, g3, g4)
 
-    def horizontal_recursion(self, r, g1, g2, g3, g4, g5):
-        r_1 = g2.coordinates
-        r_2 = g3.coordinates
-        out1 = self.hgp_begin_horizontal(g1, g3, g4, g5)
-        out2 = (r_1[r] - r_2[r]) * self.hgp_begin_horizontal(g2, g3, g4, g5)
+    def horizontal_recursion(self, r, g1, g2, g3, g4):
+        g1a1 = g2m1 = None
+
+        d_1 = g1.contraction
+        d_2 = g2.contraction
+        a_1 = g1.exponent
+        a_2 = g2.exponent
+        r_1 = g1.coordinates
+        r_2 = g2.coordinates
+        l_1 = g1.integral_exponents
+        l_2 = g2.integral_exponents
+
+        if r == 0:
+            g1a1 = PrimitiveBasis(d_1, a_1, r_1, (l_1[0] + 1, l_1[1], l_1[2]))
+            g2m1 = PrimitiveBasis(d_2, a_2, r_2, (l_2[0] - 1, l_2[1], l_2[2]))
+        elif r == 1:
+            g1a1 = PrimitiveBasis(d_1, a_1, r_1, (l_1[0], l_1[1] + 1, l_1[2]))
+            g2m1 = PrimitiveBasis(d_2, a_2, r_2, (l_2[0], l_2[1] - 1, l_2[2]))
+        elif r == 2:
+            g1a1 = PrimitiveBasis(d_1, a_1, r_1, (l_1[0], l_1[1], l_1[2] + 1))
+            g2m1 = PrimitiveBasis(d_2, a_2, r_2, (l_2[0], l_2[1], l_2[2] - 1))
+
+        r_2m1 = g2m1.coordinates
+        out1 = self.hgp_begin_horizontal(g1a1, g2m1, g3, g4)
+        out2 = (r_1[r] - r_2m1[r]) * self.hgp_begin_horizontal(g1, g2m1, g3, g4)
         return out1 + out2
 
     def hgp_begin_vertical(self, m, g1, g2, g3, g4):
@@ -69,7 +122,7 @@ class HeadGordonPople:
         if l_3[2] > 0:
             return self.vertical_recursion(2, m, *self.hgp_vertical_factory(2, g3, g4, g1, g2))
         else:
-            return self.hgp_end(m, g1, g2, g3, g4)
+            return self.end_dict[m]
 
     def vertical_recursion(self, r, m, g1, g2, g3, g4, g5, g6):
         out3 = out4 = out5 = 0
@@ -80,53 +133,26 @@ class HeadGordonPople:
         a_4 = g4.exponent
         a_5 = a_1 + a_2
         a_6 = a_3 + a_4
-        a_7 = (a_5 * a_6) / (a_5 + a_6)
 
         r_1 = g1.coordinates
         r_2 = g2.coordinates
-        r_3 = g3.coordinates
-        r_4 = g4.coordinates
         r_5 = gaussian_product_coordinate(a_1, r_1, a_2, r_2)
-        r_6 = gaussian_product_coordinate(a_3, r_3, a_4, r_4)
-        r_7 = gaussian_product_coordinate(a_5, r_5, a_6, r_6)
 
         out1 = (r_5[r] - r_1[r]) * self.hgp_begin_vertical(m, g1, g2, g3, g4)
-        out2 = (r_7[r] - r_5[r]) * self.hgp_begin_vertical((m+1), g1, g2, g3, g4)
+        out2 = (self.r_7[r] - r_5[r]) * self.hgp_begin_vertical((m+1), g1, g2, g3, g4)
         if g5.integral_exponents[r] >= 0:
             out3 = self.os_count(g1.integral_exponents[r]) * (1 / (2 * a_5)) * self.hgp_begin_vertical(m, g5, g2, g3, g4)
-            out4 = - self.os_count(g1.integral_exponents[r]) * (a_7 / (2 * a_5**2)) * self.hgp_begin_vertical((m+1), g5, g2, g3, g4)
+            out4 = self.os_count(g1.integral_exponents[r]) * (self.a_7 / (2 * a_5**2)) * self.hgp_begin_vertical((m+1), g5, g2, g3, g4)
         if g6.integral_exponents[r] >= 0:
             out5 = self.os_count(g3.integral_exponents[r]) * (1 / (2*(a_5 + a_6))) * self.hgp_begin_vertical((m+1), g1, g2, g6, g4)
 
-        return out1 + out2 + out3 + out4 + out5
+        return out1 + out2 + out3 - out4 + out5
 
     def os_count(self, i):
         if i == 0:
             return 1
         else:
             return i
-
-    def hgp_horizontal_factory(self, r, g1, g2, g3, g4):
-        d_1 = g1.contraction
-        d_2 = g2.contraction
-        a_1 = g1.exponent
-        a_2 = g2.exponent
-        r_1 = g1.coordinates
-        r_2 = g2.coordinates
-        l_1 = g1.integral_exponents
-        l_2 = g2.integral_exponents
-        if r == 0:
-            g1xa1 = PrimitiveBasis(d_1, a_1, r_1, (l_1[0] + 1, l_1[1], l_1[2]))
-            g2xm1 = PrimitiveBasis(d_2, a_2, r_2, (l_2[0] - 1, l_2[1], l_2[2]))
-            return g1xa1, g1, g2xm1, g3, g4
-        elif r == 1:
-            g1ya1 = PrimitiveBasis(d_1, a_1, r_1, (l_1[0], l_1[1] + 1, l_1[2]))
-            g2ym1 = PrimitiveBasis(d_2, a_2, r_2, (l_2[0], l_2[1] - 1, l_2[2]))
-            return g1ya1, g1, g2ym1, g3, g4
-        elif r == 2:
-            g1za1 = PrimitiveBasis(d_1, a_1, r_1, (l_1[0], l_1[1], l_1[2] + 1))
-            g2zm1 = PrimitiveBasis(d_2, a_2, r_2, (l_2[0], l_2[1], l_2[2] - 1))
-            return g1za1, g1, g2zm1, g3, g4
 
     def hgp_vertical_factory(self, r, g1, g2, g3, g4):
         d_1 = g1.contraction
@@ -153,31 +179,3 @@ class HeadGordonPople:
             g1zm2 = PrimitiveBasis(d_1, a_1, r_1, (l_1[0], l_1[1], l_1[2] - 2))
             g3zm1 = PrimitiveBasis(d_3, a_3, r_3, (l_3[0], l_3[1], l_3[2] - 1))
             return g1zm1, g2, g3, g4, g1zm2, g3zm1
-
-    def hgp_end(self, m, g1, g2, g3, g4):
-        if m in self.end_dict:
-            return self.end_dict[m]
-        else:
-            a_1 = g1.exponent
-            a_2 = g2.exponent
-            a_3 = g3.exponent
-            a_4 = g4.exponent
-            a_5 = a_1 + a_2
-            a_6 = a_3 + a_4
-
-            r_1 = g1.coordinates
-            r_2 = g2.coordinates
-            r_3 = g3.coordinates
-            r_4 = g4.coordinates
-            r_5 = gaussian_product_coordinate(a_1, r_1, a_2, r_2)
-            r_6 = gaussian_product_coordinate(a_3, r_3, a_4, r_4)
-
-            r_12 = coordinate_distance(r_1, r_2)
-            r_34 = coordinate_distance(r_3, r_4)
-            r_56 = coordinate_distance(r_5, r_6)
-
-            out1 = (2 * pi**(5/2)) / (a_5 * a_6 * sqrt(a_5 + a_6))
-            out2 = exp(((- a_1 * a_2 * r_12**2) / a_5) - ((a_3 * a_4 * r_34**2) / a_6))
-            out3 = boys_function(m, (a_5 * a_6 * r_56**2) / (a_5 + a_6))
-            self.end_dict[m] = out1 * out2 * out3
-        return out1 * out2 * out3
