dc723ad162685210d5c9eadef262d3222a917a66
Chi Yang Cheng <chicheng580@gmail.com>
2017-02-11 09:56:48 +0800

    format clean

--- src/main/integrals/twoelectronrepulsion/cook_integral.py
+++ src/main/integrals/twoelectronrepulsion/cook_integral.py
@@ -4,6 +4,7 @@ from src.main.integrals import binomial_coefficient
 from src.main.factory import gaussian_product
 from math import factorial as fac
 from math import sqrt, pi, exp
+import itertools
 
 
 class ElectronRepulsion:
@@ -11,65 +12,84 @@ class ElectronRepulsion:
     def __init__(self):
         self.end_dict = {}
 
-    def integrate(self, g1, g2, g3, g4):
-        self.end_dict = {}
+    def integrate(self, basis_i, basis_j, basis_k, basis_l):
+        primitives_i = basis_i.primitive_gaussian_array
+        primitives_j = basis_j.primitive_gaussian_array
+        primitives_k = basis_k.primitive_gaussian_array
+        primitives_l = basis_l.primitive_gaussian_array
 
-        g5 = gaussian_product(g1, g2)
-        g6 = gaussian_product(g3, g4)
+        l_1 = basis_i.integral_exponents
+        l_2 = basis_j.integral_exponents
+        l_3 = basis_k.integral_exponents
+        l_4 = basis_l.integral_exponents
 
-        a_1 = g1.exponent
-        a_2 = g2.exponent
-        a_3 = g3.exponent
-        a_4 = g4.exponent
-        a_5 = g5.exponent
-        a_6 = g6.exponent
+        r_1 = basis_i.coordinates
+        r_2 = basis_j.coordinates
+        r_3 = basis_k.coordinates
+        r_4 = basis_l.coordinates
 
-        r_1 = g1.coordinates
-        r_2 = g2.coordinates
-        r_3 = g3.coordinates
-        r_4 = g4.coordinates
-        r_5 = g5.coordinates
-        r_6 = g6.coordinates
-        r_12 = coordinate_distance(r_1, r_2)
-        r_34 = coordinate_distance(r_3, r_4)
-        r_56 = coordinate_distance(r_5, r_6)
+        ans = 0.0
+        for g1, g2, g3, g4 in itertools.product(primitives_i, primitives_j, primitives_k, primitives_l):
+            self.end_dict = {}
+            c_1 = g1.contraction
+            c_2 = g2.contraction
+            c_3 = g3.contraction
+            c_4 = g4.contraction
+            n_1 = g1.normalisation
+            n_2 = g2.normalisation
+            n_3 = g3.normalisation
+            n_4 = g4.normalisation
+            contraction = c_1 * c_2 * c_3 * c_4 * n_1 * n_2 * n_3 * n_4
 
-        l_1 = g1.integral_exponents
-        l_2 = g2.integral_exponents
-        l_3 = g3.integral_exponents
-        l_4 = g4.integral_exponents
-        l_5 = g5.integral_exponents
-        l_6 = g6.integral_exponents
+            g5 = gaussian_product(g1, g2)
+            g6 = gaussian_product(g3, g4)
 
-        delta = (1/(4*a_5)) + (1/(4*a_6))
+            a_1 = g1.exponent
+            a_2 = g2.exponent
+            a_3 = g3.exponent
+            a_4 = g4.exponent
+            a_5 = g5.exponent
+            a_6 = g6.exponent
 
-        ans = 0
-        for l in range(l_5[0] + 1):
-            for r in range(int(l/2) + 1):
-                for ll in range(l_6[0] + 1):
-                    for rr in range(int(ll/2) + 1):
-                        for i in range(int((l + ll - 2*r - 2*rr) / 2) + 1):
-                            out1 = self.b_function(l, ll, r, rr, i, l_1[0], l_2[0], r_1[0], r_2[0], r_5[0], a_5, l_3[0], l_4[0], r_3[0], r_4[0], r_6[0], a_6)
-                            for m in range(l_5[1] + 1):
-                                for s in range(int(m/2) + 1):
-                                    for mm in range(l_6[1] + 1):
-                                        for ss in range(int(mm/2) + 1):
-                                            for j in range(int((m + mm - 2*s - 2*ss) / 2) + 1):
-                                                out2 = self.b_function(m, mm, s, ss, j, l_1[1], l_2[1], r_1[1], r_2[1], r_5[1], a_5, l_3[1], l_4[1], r_3[1], r_4[1], r_6[1], a_6)
-                                                for n in range(l_5[2] + 1):
-                                                    for t in range(int(n/2) + 1):
-                                                        for nn in range(l_6[2] + 1):
-                                                            for tt in range(int(nn/2) + 1):
-                                                                for k in range(int((n + nn - 2*t - 2*tt) / 2) + 1):
-                                                                    out3 = self.b_function(n, nn, t, tt, k, l_1[2], l_2[2], r_1[2], r_2[2], r_5[2], a_5, l_3[2], l_4[2], r_3[2], r_4[2], r_6[2], a_6)
-                                                                    v = l + ll + m + mm + n + nn - 2*(r + rr + s + ss + t + tt) - (i + j + k)
-                                                                    if v in self.end_dict:
-                                                                        out4 = self.end_dict[v]
-                                                                    else:
-                                                                        out4 = boys_function(v, (r_56**2 / (4 * delta)))
-                                                                        self.end_dict[v] = out4
-                                                                    ans += out1 * out2 * out3 * out4
-        ans *= self.gaussian_product_factor(a_1, a_2, a_3, a_4, a_5, a_6, r_12, r_34)
+            r_5 = g5.coordinates
+            r_6 = g6.coordinates
+            r_12 = coordinate_distance(r_1, r_2)
+            r_34 = coordinate_distance(r_3, r_4)
+            r_56 = coordinate_distance(r_5, r_6)
+
+            l_5 = g5.integral_exponents
+            l_6 = g6.integral_exponents
+
+            delta = (1/(4*a_5)) + (1/(4*a_6))
+
+            out_5 = 0
+            for l in range(l_5[0] + 1):
+                for r in range(int(l/2) + 1):
+                    for ll in range(l_6[0] + 1):
+                        for rr in range(int(ll/2) + 1):
+                            for i in range(int((l + ll - 2*r - 2*rr) / 2) + 1):
+                                out1 = self.b_function(l, ll, r, rr, i, l_1[0], l_2[0], r_1[0], r_2[0], r_5[0], a_5, l_3[0], l_4[0], r_3[0], r_4[0], r_6[0], a_6)
+                                for m in range(l_5[1] + 1):
+                                    for s in range(int(m/2) + 1):
+                                        for mm in range(l_6[1] + 1):
+                                            for ss in range(int(mm/2) + 1):
+                                                for j in range(int((m + mm - 2*s - 2*ss) / 2) + 1):
+                                                    out2 = self.b_function(m, mm, s, ss, j, l_1[1], l_2[1], r_1[1], r_2[1], r_5[1], a_5, l_3[1], l_4[1], r_3[1], r_4[1], r_6[1], a_6)
+                                                    for n in range(l_5[2] + 1):
+                                                        for t in range(int(n/2) + 1):
+                                                            for nn in range(l_6[2] + 1):
+                                                                for tt in range(int(nn/2) + 1):
+                                                                    for k in range(int((n + nn - 2*t - 2*tt) / 2) + 1):
+                                                                        out3 = self.b_function(n, nn, t, tt, k, l_1[2], l_2[2], r_1[2], r_2[2], r_5[2], a_5, l_3[2], l_4[2], r_3[2], r_4[2], r_6[2], a_6)
+                                                                        v = l + ll + m + mm + n + nn - 2*(r + rr + s + ss + t + tt) - (i + j + k)
+                                                                        if v in self.end_dict:
+                                                                            out4 = self.end_dict[v]
+                                                                        else:
+                                                                            out4 = boys_function(v, (r_56**2 / (4 * delta)))
+                                                                            self.end_dict[v] = out4
+                                                                        out_5 += out1 * out2 * out3 * out4
+            out_5 *= self.gaussian_product_factor(a_1, a_2, a_3, a_4, a_5, a_6, r_12, r_34)
+            ans += contraction * out_5
         return ans
 
     def b_function(self, l, ll, r, rr, i, l_1, l_2, a_x, b_x, p_x, g_1, l_3, l_4, c_x, d_x, q_x, g_2):
--- src/main/integrals/twoelectronrepulsion/head_gordon_pople.py
+++ src/main/integrals/twoelectronrepulsion/head_gordon_pople.py
@@ -3,7 +3,9 @@ from src.main.common import gaussian_product_coordinate
 from src.main.integrals import boys_function
 from src.main.integrals import boys_function_recursion
 from src.main.objects import PrimitiveBasis
+from src.main.objects import Basis
 from math import sqrt, pi, exp
+import itertools
 
 
 class HeadGordonPople:
@@ -13,53 +15,74 @@ class HeadGordonPople:
         self.r_7 = ()
         self.end_dict = {}
 
-    def integrate(self, g1, g2, g3, g4):
-        l_1 = g1.integral_exponents
-        l_2 = g2.integral_exponents
-        l_3 = g3.integral_exponents
-        l_4 = g4.integral_exponents
+    def integrate(self, basis_i: Basis, basis_j: Basis, basis_k: Basis, basis_l: Basis):
+        if sum(basis_i.integral_exponents) < sum(basis_j.integral_exponents) \
+        and sum(basis_k.integral_exponents) < sum(basis_l.integral_exponents):
+            basis_i, basis_j, basis_k, basis_l = basis_j, basis_i, basis_l, basis_k
+        elif sum(basis_i.integral_exponents) < sum(basis_j.integral_exponents):
+            basis_i, basis_j, basis_k, basis_l = basis_j, basis_i, basis_k, basis_l
+        elif sum(basis_k.integral_exponents) < sum(basis_l.integral_exponents):
+            basis_i, basis_j, basis_k, basis_l = basis_j, basis_i, basis_k, basis_l
+
+        l_1 = basis_i.integral_exponents
+        l_2 = basis_j.integral_exponents
+        l_3 = basis_k.integral_exponents
+        l_4 = basis_l.integral_exponents
         l_total = sum(l_1) + sum(l_2) + sum(l_3) + sum(l_4)
 
-        a_1 = g1.exponent
-        a_2 = g2.exponent
-        a_3 = g3.exponent
-        a_4 = g4.exponent
-        a_5 = a_1 + a_2
-        a_6 = a_3 + a_4
-        self.a_7 = (a_5 * a_6) / (a_5 + a_6)
+        r_1 = basis_i.coordinates
+        r_2 = basis_j.coordinates
+        r_3 = basis_k.coordinates
+        r_4 = basis_l.coordinates
 
-        r_1 = g1.coordinates
-        r_2 = g2.coordinates
-        r_3 = g3.coordinates
-        r_4 = g4.coordinates
-        r_5 = gaussian_product_coordinate(a_1, r_1, a_2, r_2)
-        r_6 = gaussian_product_coordinate(a_3, r_3, a_4, r_4)
-        self.r_7 = gaussian_product_coordinate(a_5, r_5, a_6, r_6)
+        primitives_i = basis_i.primitive_gaussian_array
+        primitives_j = basis_j.primitive_gaussian_array
+        primitives_k = basis_k.primitive_gaussian_array
+        primitives_l = basis_l.primitive_gaussian_array
 
-        r_12 = coordinate_distance(r_1, r_2)
-        r_34 = coordinate_distance(r_3, r_4)
-        r_56 = coordinate_distance(r_5, r_6)
+        ans = 0.0
+        for g1, g2, g3, g4 in itertools.product(primitives_i, primitives_j, primitives_k, primitives_l):
+            c_1 = g1.contraction
+            c_2 = g2.contraction
+            c_3 = g3.contraction
+            c_4 = g4.contraction
+            n_1 = g1.normalisation
+            n_2 = g2.normalisation
+            n_3 = g3.normalisation
+            n_4 = g4.normalisation
+            contraction = c_1 * c_2 * c_3 * c_4 * n_1 * n_2 * n_3 * n_4
 
-        boys_x = (a_5 * a_6 * r_56**2) / (a_5 + a_6)
-        boys_out1 = (2 * pi**(5/2)) / (a_5 * a_6 * sqrt(a_5 + a_6))
-        boys_out2 = exp(((- a_1 * a_2 * r_12**2) / a_5) - ((a_3 * a_4 * r_34**2) / a_6))
-        boys_out3 = boys_function(l_total, boys_x)
+            a_1 = g1.exponent
+            a_2 = g2.exponent
+            a_3 = g3.exponent
+            a_4 = g4.exponent
+            a_5 = a_1 + a_2
+            a_6 = a_3 + a_4
+            self.a_7 = (a_5 * a_6) / (a_5 + a_6)
 
-        self.end_dict = {l_total: boys_out1 * boys_out2 * boys_out3}
+            r_5 = gaussian_product_coordinate(a_1, r_1, a_2, r_2)
+            r_6 = gaussian_product_coordinate(a_3, r_3, a_4, r_4)
+            self.r_7 = gaussian_product_coordinate(a_5, r_5, a_6, r_6)
 
-        while l_total >= 1:
-            boys_out3 = boys_function_recursion(l_total, boys_x, boys_out3)
-            l_total -= 1
-            self.end_dict[l_total] = boys_out1 * boys_out2 * boys_out3
+            r_12 = coordinate_distance(r_1, r_2)
+            r_34 = coordinate_distance(r_3, r_4)
+            r_56 = coordinate_distance(r_5, r_6)
 
-        if sum(l_1) >= sum(l_2) and sum(l_3) >= sum(l_4):
-            return self.hgp_begin_horizontal(g1, g2, g3, g4)
-        elif sum(l_1) >= sum(l_2):
-            return self.hgp_begin_horizontal(g1, g2, g4, g3)
-        elif sum(l_3) >= sum(l_4):
-            return self.hgp_begin_horizontal(g2, g1, g3, g4)
-        else:
-            return self.hgp_begin_horizontal(g2, g1, g4, g3)
+            boys_x = (a_5 * a_6 * r_56**2) / (a_5 + a_6)
+            boys_out1 = (2 * pi**(5/2)) / (a_5 * a_6 * sqrt(a_5 + a_6))
+            boys_out2 = exp(((- a_1 * a_2 * r_12**2) / a_5) - ((a_3 * a_4 * r_34**2) / a_6))
+            boys_out3 = boys_function(l_total, boys_x)
+            self.end_dict = {l_total: boys_out1 * boys_out2 * boys_out3}
+
+            m = l_total
+            while m >= 1:
+                boys_out3 = boys_function_recursion(m, boys_x, boys_out3)
+                m -= 1
+                self.end_dict[m] = boys_out1 * boys_out2 * boys_out3
+
+            ans += contraction * self.hgp_begin_horizontal(g2, g1, g4, g3)
+
+        return ans
 
     def hgp_begin_horizontal(self, g1, g2, g3, g4):
         l_2 = g2.integral_exponents
@@ -81,6 +104,7 @@ class HeadGordonPople:
 
     def horizontal_recursion(self, r, g1, g2, g3, g4):
         g1a1 = g2m1 = None
+        out2 = 0
 
         d_1 = g1.contraction
         d_2 = g2.contraction
@@ -101,9 +125,9 @@ class HeadGordonPople:
             g1a1 = PrimitiveBasis(d_1, a_1, r_1, (l_1[0], l_1[1], l_1[2] + 1))
             g2m1 = PrimitiveBasis(d_2, a_2, r_2, (l_2[0], l_2[1], l_2[2] - 1))
 
-        r_2m1 = g2m1.coordinates
         out1 = self.hgp_begin_horizontal(g1a1, g2m1, g3, g4)
-        out2 = (r_1[r] - r_2m1[r]) * self.hgp_begin_horizontal(g1, g2m1, g3, g4)
+        if r_1[r] != r_2[r]:
+            out2 = (r_1[r] - r_2[r]) * self.hgp_begin_horizontal(g1, g2m1, g3, g4)
         return out1 + out2
 
     def hgp_begin_vertical(self, m, g1, g2, g3, g4):
@@ -125,7 +149,7 @@ class HeadGordonPople:
             return self.end_dict[m]
 
     def vertical_recursion(self, r, m, g1, g2, g3, g4, g5, g6):
-        out3 = out4 = out5 = 0
+        out1 = out2 = out3 = out4 = out5 = 0
 
         a_1 = g1.exponent
         a_2 = g2.exponent
@@ -138,8 +162,10 @@ class HeadGordonPople:
         r_2 = g2.coordinates
         r_5 = gaussian_product_coordinate(a_1, r_1, a_2, r_2)
 
-        out1 = (r_5[r] - r_1[r]) * self.hgp_begin_vertical(m, g1, g2, g3, g4)
-        out2 = (self.r_7[r] - r_5[r]) * self.hgp_begin_vertical((m+1), g1, g2, g3, g4)
+        if r_5[r] != r_1[r]:
+            out1 = (r_5[r] - r_1[r]) * self.hgp_begin_vertical(m, g1, g2, g3, g4)
+        if self.r_7[r] != r_5[r]:
+            out2 = (self.r_7[r] - r_5[r]) * self.hgp_begin_vertical((m+1), g1, g2, g3, g4)
         if g5.integral_exponents[r] >= 0:
             out3 = self.os_count(g1.integral_exponents[r]) * (1 / (2 * a_5)) * self.hgp_begin_vertical(m, g5, g2, g3, g4)
             out4 = self.os_count(g1.integral_exponents[r]) * (self.a_7 / (2 * a_5**2)) * self.hgp_begin_vertical((m+1), g5, g2, g3, g4)
