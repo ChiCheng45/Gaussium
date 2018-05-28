Fixed basis set normalisation issues

src/kohnsham/integration.py
@@ -15,8 +15,11 @@ class ExchangeCorrelation:
 
     def integrate(self, density_matrix, i, j):
 
+        g_1 = self.basis_set[i]
+        g_2 = self.basis_set[j]
+
         def electron_density(x, y, z):
-            density = 0
+            density = 0.0
             for a, basis_a in enumerate(self.basis_set):
                 for b, basis_b in enumerate(self.basis_set):
                     if a == b:
@@ -29,14 +32,14 @@ class ExchangeCorrelation:
             x = rho * np.sin(theta) * np.cos(phi)
             y = rho * np.sin(theta) * np.sin(phi)
             z = rho * np.cos(theta)
-            return self.basis_set[i].value(x, y, z) * (self.exchange_potential.calculate(electron_density(x, y, z))
-            + self.correlation_potential.calculate(electron_density(x, y, z))) * self.basis_set[j].value(x, y, z) \
+            return g_1.value(x, y, z) * (self.exchange_potential.calculate(electron_density(x, y, z))
+            + self.correlation_potential.calculate(electron_density(x, y, z))) * g_2.value(x, y, z) \
             * rho**2
 
         def integrand_spherical(rho):
             return quadpy.sphere.integrate_spherical(
                 lambda azimuthal, polar: integrand(rho, polar, azimuthal),
-                rule=quadpy.sphere.Lebedev(19)
+                rule=quadpy.sphere.Lebedev(21)
             )
 
         return integrate.quad(integrand_spherical, 0.0, self.int_space, epsabs=self.epsabs, epsrel=self.epsrel,

src/objects/basis_function.py
@@ -1,3 +1,7 @@
+from scipy.misc import factorial2
+from math import sqrt, pi
+import itertools
+
 class Basis:
     """Contracted Gaussian basis function.
 
@@ -12,6 +16,39 @@ class Basis:
         self.primitive_gaussian_array = primitive_gaussian_array
         self.coordinates = coordinates
         self.integral_exponents = integral_exponents
+        self.normalisation_memo = None
+
+    @property
+    def normalisation(self):
+        """Calculates normalisation constant for the basis set and stores in self.normalisation once called.
+
+        Returns
+        -------
+        self.normalisation_memo : float
+
+        """
+        if self.normalisation_memo is None:
+            l, m, n = self.integral_exponents
+
+            if len(self.primitive_gaussian_array) == 1:
+                self.normalisation_memo = 1
+            else:
+                ans = 0.0
+                for primitive_a, primitive_b in itertools.product(self.primitive_gaussian_array, repeat=2):
+                    a_1 = primitive_a.exponent
+                    a_2 = primitive_b.exponent
+                    c_1 = primitive_a.contraction
+                    c_2 = primitive_b.contraction
+                    n_1 = primitive_a.normalisation
+                    n_2 = primitive_b.normalisation
+
+                    out1 = factorial2(2 * l - 1) * factorial2(2 * m - 1) * factorial2(2 * n - 1)
+                    out2 = (pi / (a_1 + a_2)) ** (3 / 2)
+                    out3 = (2 * (a_1 + a_2)) ** (l + m + n)
+                    ans += (c_1 * c_2 * n_1 * n_2 * out1 * out2) / out3
+                self.normalisation_memo = 1 / sqrt(ans)
+
+        return self.normalisation_memo
 
     def value(self, x, y, z):
         """Returns the value at point x, y, z.
@@ -29,4 +66,4 @@ class Basis:
         ans = 0
         for primitive in self.primitive_gaussian_array:
             ans += primitive.value(x, y, z)
-        return ans
+        return self.normalisation * ans

src/objects/primitive_basis_function.py
@@ -1,5 +1,5 @@
 from math import pi, sqrt
-from math import factorial as fac
+from scipy.misc import factorial2
 import numpy as np
 
 
@@ -25,7 +25,7 @@ class PrimitiveBasis:
 
     @property
     def normalisation(self):
-        """Calculates normalisation constant and stores in self.normalisation once called.
+        """Calculates normalisation constant for the primitive gaussian and stores in self.normalisation once called.
 
         Returns
         -------
@@ -33,11 +33,11 @@ class PrimitiveBasis:
 
         """
         if self.normalisation_memo is None:
-            x, y, z = self.integral_exponents
-            out1 = ((2 * self.exponent) / pi) ** (3 / 4)
-            out2 = (8 * self.exponent) ** (x + y + z) * fac(x) * fac(y) * fac(z)
-            out3 = fac(2 * x) * fac(2 * y) * fac(2 * z)
-            self.normalisation_memo = out1 * sqrt(out2 / out3)
+            l, m, n = self.integral_exponents
+            out1 = factorial2(2 * l - 1) * factorial2(2 * m - 1) * factorial2(2 * n - 1)
+            out2 = (pi / (2 * self.exponent))**(3/2)
+            out3 = (4 * self.exponent)**(l + m + n)
+            self.normalisation_memo = 1 / sqrt((out1 * out2) / out3)
         return self.normalisation_memo
 
     def value(self, x, y, z):